17ed02e9a0f1bf502406af70d12bb01dddef348f
Ana Júlia <anajulia99@gmail.com>
2020-10-31 13:22:34 -0300

    Update Trabalho 1 - Sistema Operacionais.cpp
    Criação de funções com o código no main.

--- Trabalho 1 - Sistema Operacionais.cpp
+++ Trabalho 1 - Sistema Operacionais.cpp
@@ -15,145 +15,179 @@
 
 #define Max 500
 
+//Alunos Ana Júlia Valverde e Daniel Almeida
+
 using namespace std;
 
-        struct registro {
-          int p; //prioridade do processo
-          int c; //o instante de chegada do processo
-          int s; //o tempo de serviço do processo
-          int esp=-1; //variavel para somar o tempo de espera
-          int resp=-1; //somar o tempo resposta
-          int t; //tempo de serviço
-          int com;
-          int fim;
-        };
+struct registro {
+  int p; //prioridade do processo
+  int c; //o instante de chegada do processo
+  int s; //o tempo de serviço do processo
+  int esp=-1; //variavel para somar o tempo de espera
+  int resp=-1; //somar o tempo resposta
+  int t; //tempo de serviço
+  int com;
+  int fim;
+};
 
-        struct resultado {
-          float espe; //retorna espera
-          float respo; //retorna resposta
-        };
+struct resultado {
+  float espe; //retorna espera
+  float respo; //retorna resposta
+};
 
-        resultado funcaoPRIO(list<registro> lista);
-        resultado funcaoRRQ5(list<registro> lista);
-        void lerArquivo(string nome, string vetor[Max]);
-        bool comparaC(registro a, registro b);
-        bool comparaS(registro a, registro b);
-        bool comparaP(registro a, registro b);
-        void salvarArquivo(float matrizSalv[4][2]);
-        int converte(string s);
-        char linha[Max];
-        char *palavra= strtok(linha," ");
-        int tamL=0;
+void salvarArquivo(float matrizSalv[4][2]);
+int lerArquivo(string nome);
+float funcaoFIFO(list<registro> lista);
+float funcaoSRT(list<registro> lista);
+resultado funcaoPRIO(list<registro> lista);
+resultado funcaoRRQ5(list<registro> lista);
+bool comparaS(registro a, registro b);
+bool comparaC(registro a, registro b);
+bool comparaP(registro a, registro b);
+int converte(string s);
+int alteraValor(int nume);
+char linha[Max];
+char *palavra= strtok(linha," ");
+int tamL=0;
+string vetPalavras[Max];
 
 
 int main(){
 
     setlocale(LC_ALL,"portuguese");
-    string vetPalavras[Max], nome, vetLinhas[Max], vetSalvar[Max], f;
+    string nome, f;
+
+    int tamP, n=0;
+    float total=0;
+    list<registro>::iterator it;
+
+    float FIFO, SRT_, matrizFinal[4][2];
     resultado PRIO, RRQ5;
+    //queue<registro> fila;
+    list<registro> lista1;
+	registro aux;
 
+    cout<<"Digite o nome do arquivo de texto com .txt"<<endl;
+    cin>>nome;
 
-    int tamP, n=0, anterior=0,Ttotal,teste,j=0;
-    float total=0, matrizFinal[4][2];
-    queue<registro> fila;
-    list<registro> lista, listaAux,lista1;
-	list<registro>::iterator it,ti;
-    registro aux;
+     tamP=lerArquivo(nome);
 
-    nome="Teste.txt";
-        //Ler arquivo
-        tamP=lerArquivo(nome);
-        tamP=i;
-
-    for(int a;a<tamP;a++){
-            cout<<vetPalavras[a]<<endl;
-    }
-    
-        n = converte(vetPalavras[0]);
+    n = converte(vetPalavras[0]);
 
     for(int i=1;i<tamP;i+=3){
-
         aux.p = converte(vetPalavras[i]);
         aux.c = converte(vetPalavras[i+1]);
         aux.s = converte(vetPalavras[i+2]);
         aux.t=0;
         aux.com=0;
         aux.fim=0;
-        //j++;
-        fila.push(aux);
-        lista.push_back(aux);
+        lista1.push_back(aux);
     }
 
-
-	for(int i=0;i<n;i++){
-        cout << fila.front().p <<" " << fila.front().c <<" " << fila.front().s<<endl;
-        fila.pop();
-
-		if(fila.empty()){
-			cout << "Vazio na iteração " << i+1 << endl;
-		}
-	}
-
-    cout<<"**************************************************************"<<endl;
-	lista.sort(comparaC);
-	for(it = lista.begin(); it!=lista.end();it++){
-
-        cout<< (*it).p << " "<< (*it).c << " "<<(*it).s<< " "<<(*it).t <<endl;
-	}
-	cout<<endl<<"**************************************************************"<<endl;
-	Ttotal=lista.front().c;
-	for(it = lista.begin(); it!=lista.end();it++){
-        total=total+Ttotal-(*it).c;
-        Ttotal+=(*it).s;
-	
-
-	}
-
-
-	cout<<endl;
-	total=total/n;
-    cout.precision(2);
-    cout <<fixed << total;
-
-
-	cout<<"FIFO "<<endl<<total;
-cout<<endl<<Ttotal;
-
-matrizFinal[0][0] = total;
+    matrizFinal[0][0] = funcaoFIFO(lista1);
     matrizFinal[0][1] = matrizFinal[0][0];
-
-	lista.sort(comparaC);
-	Ttotal=lista.front().c;
-	total=0;
-	for(it = lista.begin(); it!=lista.end();it++){
-        total=total+Ttotal-(*it).c;
-        Ttotal+=(*it).s;
-        lista.sort(comparaS);
-
-	}
-
-
-	cout<<endl;
-	total=total/n;
-    cout.precision(2);
-    cout <<fixed << total;
-
-
-	cout<<"SRT_ "<<endl<<total;
-cout<<endl<<Ttotal;
-matrizFinal[2][0] = total;
-matrizFinal[2][1] = matrizFinal[2][0];
-
-    RRQ5 = funcaoRRQ5(lista);
+    RRQ5 = funcaoRRQ5(lista1);
     matrizFinal[3][0]=RRQ5.espe;
     matrizFinal[3][1]=RRQ5.respo;
-
-    PRIO = funcaoPRIO(lista);
+    matrizFinal[2][0] = funcaoSRT(lista1);
+    matrizFinal[2][1] = matrizFinal[2][0];
+    PRIO = funcaoPRIO(lista1);
     matrizFinal[1][0]=PRIO.espe;
     matrizFinal[1][1]=PRIO.respo;
     salvarArquivo(matrizFinal);
+    it--;
+    lista1.clear();
 
 
+}
+
+
+int converte(string s){
+    int num;
+    stringstream ss(s);
+    ss >> num;
+    return num;
+}
+
+float funcaoFIFO(list<registro> lista){
+    int Ttotal;
+    float total=0;
+    list<registro>::iterator it;
+    lista.sort(comparaC);
+    int n = lista.size();
+
+    Ttotal = lista.front().c;
+    for(it = lista.begin(); it!=lista.end();it++){
+        total=total+Ttotal-(*it).c;
+        Ttotal+=(*it).s;
+	}
+    total = total/n;
+
+	return total;
+}
+
+resultado funcaoRRQ5(list<registro> lista){
+    list<registro>::iterator it;
+    queue<registro> filaN;
+    registro aux1;
+    resultado resposta;
+    int n = lista.size();
+    int Stotal=0, inicio, tam=5;
+    float totalEsp=0, totalResp=0;
+
+    lista.sort(comparaC);
+    for(it = lista.begin(); it!=lista.end();it++){
+        Stotal+=(*it).s;
+	}
+	for(int i;i<=filaN.size();i++){
+        filaN.pop();
+	}
+	it = lista.begin();
+    filaN.push(lista.front());
+	inicio=(*it).c;
+    aux1=filaN.front();
+    lista.erase(it);
+	for(int i=inicio;i<Stotal+inicio;i+=tam){
+        it = lista.begin();
+        aux1.c=(*it).c;
+        while((aux1.c>i)&&(aux1.c<=i+tam)&&(lista.size()!=0)){
+            it = lista.begin();
+            filaN.push(lista.front());
+            lista.erase(it);
+            it = lista.begin();
+            aux1.c=(*it).c;
+        }
+        aux1=filaN.front();
+        filaN.pop();
+        if(aux1.esp==-1){
+            aux1.esp=aux1.c;
+        }
+        if(aux1.s>=tam){
+            totalEsp+=i-aux1.esp;
+            aux1.esp=i+tam;
+            aux1.s-=tam;
+            if(aux1.resp==-1){
+                aux1.resp=i-aux1.c;
+                totalResp+=i-aux1.c;
+            }
+            if(aux1.s!=0){
+                filaN.push(aux1);
+            }
+        }else if((aux1.s<tam)&&(aux1.s>=0)){
+            totalEsp+=i-aux1.esp;
+            aux1.esp=i+aux1.s;
+            if(aux1.resp==-1){
+                aux1.resp=i-aux1.c;
+                totalResp+=i-aux1.c;
+            }
+            i-=tam-aux1.s;
+            aux1.s=0;
+        }
+    }
+    resposta.espe = totalEsp/n;
+    resposta.respo = totalResp/n;
+
+    return resposta;
 }
 
 resultado funcaoPRIO(list<registro> lista){
@@ -233,87 +267,29 @@ resultado funcaoPRIO(list<registro> lista){
     return resposta;
 }
 
-resultado funcaoRRQ5(list<registro> lista){
+float funcaoSRT(list<registro> lista){
+    int Ttotal;
+    float total=0;
+    int j=0, n;
     list<registro>::iterator it;
-    queue<registro> filaN;
-    registro aux1;
-    resultado resposta;
-    int n = lista.size();
-    int Stotal=0, inicio, tam=5;
-    float totalEsp=0, totalResp=0;
-
-    lista.sort(comparaC);
-    for(it = lista.begin(); it!=lista.end();it++){
-        Stotal+=(*it).s;
-	}
-	for(int i;i<=filaN.size();i++){
-        filaN.pop();
-	}
-	it = lista.begin();
-    filaN.push(lista.front());
-	inicio=(*it).c;
-    aux1=filaN.front();
-    lista.erase(it);
-	for(int i=inicio;i<Stotal+inicio;i+=tam){
-        it = lista.begin();
-        aux1.c=(*it).c;
-        while((aux1.c>i)&&(aux1.c<=i+tam)&&(lista.size()!=0)){
-            it = lista.begin();
-            filaN.push(lista.front());
-            lista.erase(it);
-            it = lista.begin();
-            aux1.c=(*it).c;
-        }
-        aux1=filaN.front();
-        filaN.pop();
-        if(aux1.esp==-1){
-            aux1.esp=aux1.c;
-        }
-        if(aux1.s>=tam){
-            totalEsp+=i-aux1.esp;
-            aux1.esp=i+tam;
-            aux1.s-=tam;
-            if(aux1.resp==-1){
-                aux1.resp=i-aux1.c;
-                totalResp+=i-aux1.c;
+    list<registro> listaAux,lista1;
+    registro aux;
+        lista.sort(comparaC);
+        Ttotal=lista.front().c;
+        total=0;
+            for(it = lista.begin(); it!=lista.end();it++){
+                total=total+Ttotal-(*it).c;
+                Ttotal+=(*it).s;
+                lista.sort(comparaS);
+                if((*it).c>Ttotal){
+                     lista.sort(comparaC);
+                }
+                else if((*it).c<=Ttotal){
+                    lista.sort(comparaS);
+                }
             }
-            if(aux1.s!=0){
-                filaN.push(aux1);
-            }
-        }else if((aux1.s<tam)&&(aux1.s>=0)){
-            totalEsp+=i-aux1.esp;
-            aux1.esp=i+aux1.s;
-            if(aux1.resp==-1){
-                aux1.resp=i-aux1.c;
-                totalResp+=i-aux1.c;
-            }
-            i-=tam-aux1.s;
-            aux1.s=0;
-        }
-    }
-    resposta.espe = totalEsp/n;
-    resposta.respo = totalResp/n;
-
-    return resposta;
-}
-
-int converte(string s){
-    int num;
-    stringstream ss(s);
-    ss >> num;
-    return num;
-}
-
-
-void lerArquivo(string nome, string vetor[Max]){
-    int i=0;
-    ifstream leia(nome);
-        while(!leia.eof()){
-            leia.getline(linha,Max);
-            palavra = strtok(linha, " ");
-            vetor[i]=palavra;
-            i++;
-        }
+        total=total/lista.size();
+        return total;
 }
 
 int lerArquivo(string nome){
@@ -347,13 +323,16 @@ void salvarArquivo(float matrizSalv[4][2]){
         cout<<"Resposta salva em Resultado.txt"<<endl;
 }
 
-bool comparaC(registro a, registro b){ //Função para compara Alunos
-  return a.c < b.c ; //Aluno a vem antes do Aluno b se a nota do aluno a é menor que a nota do aluno b
+bool comparaC(registro a, registro b){ //Função ordena pela ordem de chegada
+  return a.c < b.c ;
 }
-bool comparaS(registro a, registro b){ //Função para compara Alunos
-  return a.s < b.s; //Aluno a vem antes do Aluno b se a nota do aluno a é menor que a nota do aluno b
+
+
+bool comparaS(registro a, registro b){ //Função ordena pelo tempo de serviço
+  return a.s < b.s;
 }
-bool comparaP(registro a, registro b){ //Função para compara Alunos
-  return a.p < b.p; //Aluno a vem antes do Aluno b se a nota do aluno a é menor que a nota do aluno b
+
+bool comparaP(registro a, registro b){ //Função ordena por prioridade
+  return a.p < b.p;
 }
 
